7d1d5b3fd9339b53044154ae9248b9eb50e85888
rahulsrajput <rahulsrajput29@gmail.com>
2024-04-22 15:31:42 +0530

    ..

--- BuyNotes/settings.py
+++ BuyNotes/settings.py
@@ -177,4 +177,3 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
-CSRF_TRUSTED_ORIGINS = ["https://buynotes.vercel.app/"]
